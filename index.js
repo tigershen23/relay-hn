bff2bb887d7fb098be6fd7dd4e4e490089419f01
tigershen23 <tigershen23@gmail.com>
2015-10-21 05:43:12 -0700

    Add initial index.js component

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert("hi relay!")
+import React from "react"
+import ReactDOM from "react-dom"
+
+class Item extends React.Component {
+  render() {
+    let item = this.props.store.item
+
+    return (
+      <div>
+        <h1><a href={item.url}>{item.title}</a></h1>
+        <h2>{item.score} - {item.by.id}</h2>
+        <hr />
+      </div>
+    );
+  }
+}
+
